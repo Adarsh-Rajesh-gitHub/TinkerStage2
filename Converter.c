d98f43fd99db9359686192e0a0e94af330dbc367
Adarsh Rajesh <ar77947@cs.utexas.edu>
2026-02-13 17:14:57 -0600

    memory checks

--- Converter.c
+++ Converter.c
@@ -170,7 +170,7 @@ int execute(uint32_t instruction, uint64_t *pc) {
             // if(badMem((uint64_t)address, 8)) return 1;
             //if(badMem(registers[rd] + Ls, 8)) return 1;
             if((registers[rd] + Ls)%4 != 0 || addresss < 0x1000) return 1;
-            memcpy(&memory[addresss], &registers[rs], sizeof(uint64_t)); break;
+            memcpy(&memory[registers[rd] + Ls], &registers[rs], sizeof(uint64_t)); break;
             // int64_t addrr=(int64_t)registers[rd]+convt(L);
             // if(addrr<0||addrr+8>(int64_t)sizeof(memory)||(addrr&7)) return 1;
             // memcpy(&memory[addrr],&registers[rs],8);
