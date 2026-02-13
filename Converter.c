05f97d54d2e243a8cb83f039ebd4bfad3633ab03
Adarsh Rajesh <ar77947@cs.utexas.edu>
2026-02-13 16:16:43 -0600

    memory checks

--- Converter.c
+++ Converter.c
@@ -21,7 +21,7 @@ bool badMem(uint64_t mem) {
 //check if opcode, registers, label, pc are within bounds, check if insturction is empty in right place
 int checkBounds(uint32_t instruction, uint32_t op, uint32_t rd, uint32_t rs, uint32_t rt, uint32_t L, uint64_t *pc) {
     if(rd < 0 || rd > 31 || rs < 0 || rs > 31 || rt < 0 || rt > 31
-        || op < 0 || op > 29 || L > 0xFFF || badMem(*pc)) return 1;
+        || op < 0 || op > 29 || L > 0xFFF || *pc < 0x1000 || *pc > 512*1024-5) return 1;
             switch(op) {
         case 0: if(L!=0) return 1; break;
         case 1: if(L!=0) return 1; break;
@@ -234,7 +234,6 @@ int main(int argc, char *argv[]) {
     registers[31] = sizeof(memory);
 
     while(1) {
-        if(badMem(pc)) return 1;
         uint32_t instruction;
         memcpy(&instruction, &memory[pc], sizeof(uint32_t));
         int res = execute(instruction, &pc);
